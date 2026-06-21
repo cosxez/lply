9a97465b5fb78e1e16fed4c545ca4e3c77aa6fc3
cosxez <faceless113x@gmail.com>
2026-06-22 00:54:48 +0400

    Add files via upload

---
+++ lply-GUI/cfg_pars.h
@@ -0,0 +1,61 @@
+#pragma once
+
+struct cnf
+{
+	char ip[128];
+	unsigned short port;
+}__attribute__((packed));
+
+char cfg_pars(struct cnf* conf)
+{
+	FILE *file=fopen("config","r");
+	if (file!=NULL)
+	{
+		char *cl=NULL;
+		char *arg=NULL;
+		char *pr=NULL;
+		size_t cls=0;
+		size_t prs=0;
+		
+		int rss=0;
+		while (rss!=-1)
+		{
+			rss=getline(&cl,&cls,file);
+			for (int i=0;i<rss;i++)
+			{
+				if (cl[i]==' ' || cl[i]=='=')
+				{
+					arg=(char*)malloc(i);
+					memcpy(arg,cl,i);
+					break;
+				}
+			}
+			for (int i=rss-1;i>0;i--)
+			{
+				if (cl[i]==' ' || cl[i]=='=')
+				{
+					pr=(char*)malloc(rss-i-1);
+					memcpy(pr,cl+i+1,rss-i-1);
+					prs=rss-i-2;
+					break;
+				}
+			}
+			if (strcmp(arg,"ip")==0)
+			{
+				memcpy(conf->ip,pr,prs);
+				conf->ip[prs]='\0';
+			}
+			
+			if (strcmp(arg,"port")==0)
+			{
+				conf->port=atoi(pr);
+			}
+		}
+		free(pr);
+		free(arg);
+		free(cl);
+		fclose(file);
+		return 0;
+	}
+	return 1;
+}
---
+++ lply-GUI/main.c
@@ -0,0 +1,24 @@
+#define MINIAUDIO_IMPLEMENTATION
+#include "miniaudio.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <SDL2/SDL.h>
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#include "cfg_pars.h"
+
+int main()
+{
+	struct cnf conf;
+	if (cfg_pars(&conf)!=0){return -1;}
+	
+	printf("%s\n",conf.ip);
+	printf("%d\n",conf.port);
+	return 0;
+}
